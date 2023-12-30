b3ddd4a208dfa1459a68ffb8e66d5ac38840c956
thanhpoly <117475651+thanhpoly@users.noreply.github.com>
2023-12-30 14:49:22 +0700

    Create ecommerce.is-an.app.js

---
+++ domains/ecommerce.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Sakana Music Player',
+  domain: 'is-an.app',
+  subdomain: 'smp',
+  owner: {
+    repo: 'https://github.com/vhoang140299/ecommerce',
+    email: 'vhoang140299@gmail.com',
+  },
+  record: {
+    CNAME: 'vhoang140299.github.io',
+  },
+  proxy: false,
+})
